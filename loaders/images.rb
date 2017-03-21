365a8de7ce08eb3d3b45fc33c55652b0c84218ba
MopperWhite <mopperwhite@gmail.com>
2017-03-21 21:14:22 +0800

    fix images loader

--- loaders/images.rb
+++ loaders/images.rb
@@ -2,8 +2,7 @@ on_ext :png, :jpg, :jpeg
 for_each do |src, dst|
   File.write dst, %Q{
     <img class="img-responsive center-block" src="#{
-      src.start_with?('asserts/') ?
-        as_assert(src) :
-        src}" />
+      as_assert(src)
+    }" />
   }
 end
