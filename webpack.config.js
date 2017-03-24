6e94f47dcc52e9b87ce10e2a5f94f9c92166a9a4
MopperWhite <mopperwhite@gmail.com>
2017-03-24 14:28:10 +0800

    use font-awesome cdn

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader"
+        loader: "url-loader"
       },
       {
         test: /\.json$/,
